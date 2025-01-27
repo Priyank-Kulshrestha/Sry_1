fa1b20999c667fc85bae4f1bd1467c667146ed6e
Priyank-Kulshrestha <59887380+Priyank-Kulshrestha@users.noreply.github.com>
2025-01-28 00:31:02 +0530

    first commit

--- js/date.js
+++ js/date.js
@@ -4,7 +4,7 @@ dv.style.opacity = 0;
 var val = 0;
 
 function timer(){
-	var start = new Date(2018, 0, 27, 20, 53);
+	var start = new Date(2024, 8, 15, 2, 36);
 	var t = new Date() - start;
 	var d = Math.floor(t / 1000 / 60 / 60 / 24);
 	var h = Math.floor(t / 1000 / 60 / 60 % 24);
@@ -49,3 +49,4 @@ fadeInterval = setInterval(function(){
 		fadeinInterval = setInterval(fadein, 50);
 	}
 }, 50)
+
